e76b03a0e2eae0946c26cb96ee61e33d0c1c2446
waterprisem <103801679+waterprisem@users.noreply.github.com>
2026-04-25 16:02:30 -0400

    joystick input controls a player position
    does not update screen yet

--- screen_cursor/screen_cursor.ino
+++ screen_cursor/screen_cursor.ino
@@ -3,12 +3,14 @@
 #include <Fonts/Picopixel.h>
 #include <Fonts/FreeMonoBoldOblique24pt7b.h>
 
-//#define EPD_CS    10
-//#define EPD_RST   9
-//#define EPD_BUSY  8
+#define EPD_CS    10
+#define EPD_RST   9
+#define EPD_BUSY  8
 
+int cursorX = 0;
+int cursorY = 0;
 
-
+int update_screen_cursor();
 
 //Joystick related stuff
 const int VRX_PIN = 7; // Joystick X-axis
@@ -20,32 +22,30 @@ long ySum = 0;
 int xCenter, yCenter;
 const int samples = 100;
 
-//GxEPD2_BW<GxEPD2_it60_1448x1072, GxEPD2_it60_1448x1072::HEIGHT / 8> display(GxEPD2_it60_1448x1072(/*CS=*/10, /*DC=*/-1, /*RST=*/9, /*BUSY=*/8));
+GxEPD2_BW<GxEPD2_it60_1448x1072, GxEPD2_it60_1448x1072::HEIGHT / 8> display(GxEPD2_it60_1448x1072(/*CS=*/10, /*DC=*/-1, /*RST=*/9, /*BUSY=*/8));
 
 void setup() {
   Serial.begin(115200);
+  
+  SPI.begin(12, 13, 11, 10); // SCK, MISO, MOSI, CS
+  delay(100); // give the IT8951 time to power up
+  display.init(115200, true, 1000, false, SPI, SPISettings(4000000, MSBFIRST, SPI_MODE0));
+  display.setRotation(90);
+  display.setFont(&FreeMonoBoldOblique24pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFullWindow();
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(100, 100);
+    display.print("Hello Worldddd!");
+  } while (display.nextPage());
+  Serial.println("Done!");
+
 
   pinMode(SW_PIN, INPUT_PULLUP); //Joystick button pin
 
   calibrateJoystick();
-
-  
-  // SPI.begin(12, 13, 11, 10); // SCK, MISO, MOSI, CS
-  // delay(100); // give the IT8951 time to power up
-  // display.init(115200, true, 50, false, SPI, SPISettings(4000000, MSBFIRST, SPI_MODE0));
-
-  // display.setRotation(90);
-  // display.setFont(&FreeMonoBoldOblique24pt7b);
-  // display.setTextColor(GxEPD_BLACK);
-  // display.setFullWindow();
-  // display.firstPage();
-  // do {
-  //   display.fillScreen(GxEPD_WHITE);
-  //   display.setCursor(100, 100);
-  //   display.print("Hello World!");
-  // } while (display.nextPage());
-
-  // Serial.println("Done!");
 
 }
 
@@ -66,7 +66,22 @@ void loop() {
   yVal = -constrain(yVal, -100, 100);
 
   Serial.printf("X: %d | Y: %d\n", xVal, yVal);
-  delay(100);
+
+  if (xVal != 0 || yVal != 0) { // Changed 'OR' to '||'
+    update_screen_cursor(xVal, yVal);
+  }
+  delay(300);
+}
+
+void update_screen_cursor(int x_change, int y_change) { // Added 'int' types
+  Serial.printf("Old Cursor X: %d | Old Cursor Y: %d\n", cursorX, cursorY);
+  cursorX += x_change; 
+  cursorY += y_change;
+  
+  cursorX = constrain(cursorX, 0, display.width());
+  cursorY = constrain(cursorY, 0, display.height());
+
+  Serial.printf("New Cursor X: %d | New Cursor Y: %d\n", cursorX, cursorY);
 }
 
 
